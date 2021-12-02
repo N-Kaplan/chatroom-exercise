752400df728966f93f2dd62ee5a12102459b5978
Naama Kaplan <nkaplany@gmail.com>
2021-12-02 10:52:05 +0100

    server set up and can connect to multiple clients

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+
+const app = express(); //defines application
+const clientPath = `${__dirname}/../client`; //gives path to client
+
+app.use(express.static(clientPath)); //use express to host the client
+const server = http.createServer(app); //use http to serve the app that express provides
+
+//get server live
+server.listen(8080, () => {
+    console.log("server running on " + 8080);
+});
+
+const io = require('socket.io')(server); //io variable is now the entry point of all sockets connected to the server
+
+//verify connection
+
+let counter = 0;
+
+io.on('connection', (socket) => {
+    counter++;
+    console.log(counter + ' someone connected');
+});
